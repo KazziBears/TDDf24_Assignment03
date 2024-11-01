a7bee13979e1a46807ddadc465186cd5bc4e9a93
KazziBears <justinadakin@gmail.com>
2024-11-01 17:26:20 -0400

    Edited function to pass B.

--- Project1/rpstools.c
+++ Project1/rpstools.c
@@ -2,8 +2,13 @@
 
 int CheckResults(char* player01, char* player02) {
 	if ((player01 == "ROCK" && player02 == "SCISSORS") ||
-		(player01 == "PAPER" && player02 == "ROCK") ||
-		(player01 == "SCISSORS" && player02 == "PAPER")) {
+			(player01 == "PAPER" && player02 == "ROCK") ||
+			(player01 == "SCISSORS" && player02 == "PAPER")) {
 		return 1;
+	}
+	else if ((player01 == "ROCK" && player02 == "PAPER") ||
+		(player01 == "PAPER" && player02 == "SCISSORS") ||
+		(player01 == "SCISSORS" && player02 == "ROCK")) {
+		return 2;
 	}
 }
