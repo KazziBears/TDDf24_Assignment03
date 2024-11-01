797a82d3e128ffcb80aabbf5dc1c273401de4f3b
KazziBears <justinadakin@gmail.com>
2024-11-01 16:23:04 -0400

    Added main.

---
+++ rpstools.Tests/rpstools.Tests.cpp
@@ -0,0 +1,15 @@
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace rpstoolsTests
+{
+	TEST_CLASS(rpstoolsTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
