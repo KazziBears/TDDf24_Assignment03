c85627aa98b3d09d25b91c69c4941136a8894db7
KazziBears <justinadakin@gmail.com>
2024-11-01 16:56:19 -0400

    Created test A.

--- rpstools.Tests/rpstools.Tests.cpp
+++ rpstools.Tests/rpstools.Tests.cpp
@@ -1,15 +1,29 @@
 #include "CppUnitTest.h"
 
+extern "C" {
+#include "../Project1/rpstools.h"
+}
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
 namespace rpstoolsTests
 {
-	TEST_CLASS(rpstoolsTests)
+	TEST_CLASS(FunctionTests)
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(CheckResultsTestA)
 		{
+			//Arrange
+			char* player01 = "ROCK";
+			char* player02 = "SCISSORS";
+			int expected = 1;
+
+			//Act
+			int actual = CheckResults(player01, player02);
+
+			//Assert
+			Assert::AreEqual(expected, actual);
 		}
 	};
 }
